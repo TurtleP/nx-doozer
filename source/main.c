87dc48dd9ff5f840bde1c9b7867dcec0bf306a80
= <jeremy.postelnek@gmail.com>
2018-05-10 13:47:06 -0400

    doozer test

--- source/main.c
+++ source/main.c
@@ -5,25 +5,33 @@
 
 int main(int argc, char **argv)
 {
-	gfxInitDefault();
+    gfxInitDefault();
 
-	while(appletMainLoop())
-	{
-		//Scan all the inputs. This should be done once for each frame
-		hidScanInput();
+    //Initialize console. Using NULL as the second argument tells the console library to use the internal console structure as current one.
+    consoleInit(NULL);
 
-		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
-		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+    //Move the cursor to row 16 and column 20 and then prints "Hello World!"
+    //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
+    //the row and column where you want your cursor to move
+    printf("\x1b[16;20HHello World!");
 
-		if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
+    while(appletMainLoop())
+    {
+        //Scan all the inputs. This should be done once for each frame
+        hidScanInput();
 
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-		gfxWaitForVsync();
-	}
+        //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
-	gfxExit();
+        if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
 
-	return 0;
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        gfxWaitForVsync();
+    }
+
+    gfxExit();
+    return 0;
 }
+
 
